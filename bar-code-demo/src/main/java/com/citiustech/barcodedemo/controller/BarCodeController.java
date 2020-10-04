1eea69b892f662c7b142868b501021599ff5db50
bablu.rajbhar87@gmail.com <bablu.rajbhar87@gmail.com>
2020-10-04 14:53:36 -0700

    Added code to create a project for making a bar-code

---
+++ bar-code-demo/src/main/java/com/citiustech/barcodedemo/controller/BarCodeController.java
@@ -0,0 +1,28 @@
+package com.citiustech.barcodedemo.controller;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.image.BufferedImage;
+
+
+@RestController
+@RequestMapping("/barcodes")
+public class BarCodeController {
+
+    @RequestMapping(value = "/qrcode/generate/{text}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<BufferedImage> generateQRCodeImage(@PathVariable() String text) throws WriterException {
+        QRCodeWriter writer = new QRCodeWriter();
+        BitMatrix matrix = writer.encode("Hello", BarcodeFormat.QR_CODE, 200,200);
+        return ResponseEntity.ok(MatrixToImageWriter.toBufferedImage(matrix));
+    }
+
+}
---
+++ bar-code-demo/src/test/java/com/citiustech/barcodedemo/BarCodeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citiustech.barcodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarCodeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
